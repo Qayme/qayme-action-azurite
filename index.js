d8589e68adea58ec862c8c90d9dcef037503a2be
Lukas Holota <l.holota@outlook.com>
2022-06-14 19:56:01 +0200

    Fix

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ async function run() {
         const startTimeout = parseInt(core.getInput('start-timeout')) * 1000;
         const imageTag = core.getInput('image-tag');
 
-        await exec.exec(`docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 mcr.microsoft.com/azure-storage/azurite:${imageTag}`)
+        const fullImageName = `mcr.microsoft.com/azure-storage/azurite:${imageTag}`
+
+        // This is to avoid countint the time to pull the image into the start timeout
+        await exec.exec(`docker pull ${fullImageName}`);
+        await exec.exec(`docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 ${fullImageName}`);
 
         try {
             await waitUntil(
@@ -20,7 +24,6 @@ async function run() {
                 });
         }
         catch (ex) {
-            console.error("Azurite did not get ready in time. Please see the log below:");
             core.setFailed("Azurite did not get ready in time.");
         }
     }
