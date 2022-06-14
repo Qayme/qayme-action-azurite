4fbce6a7e0b0bbdd7ab4a19f1944ecced712cf48
Lukas Holota <l.holota@outlook.com>
2022-06-14 16:06:29 +0200

    Test

---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require("child_process");
+
+function run() {
+    try {
+        // execute bash script
+        // `who-to-greet` input defined in action metadata file
+        // const nameToGreet = core.getInput('who-to-greet');
+        // console.log(`Hello ${nameToGreet}!`);
+        // const time = (new Date()).toTimeString();
+        // core.setOutput("time", time);
+        // // Get the JSON webhook payload for the event that triggered the workflow
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log(`The event payload: ${payload}`);
+        console.log("Sample action executed!");
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function cleanup() {
+    try {
+        console.log("Clean up executed!");
+    } catch (error) {
+        core.warning("clean up failed...")
+    }
+}
