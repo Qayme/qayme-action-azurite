e62e580fc096abb90bd0427f67803c37721275e3
Lukas Holota <l.holota@outlook.com>
2022-06-14 17:18:45 +0200

    Spawn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+//const exec = require('@actions/exec');
+const child = require("child_process");
 const { waitUntil } = require('async-wait-until/dist/commonjs');
 
 async function run() {
@@ -10,34 +11,58 @@ async function run() {
         var blobsReady = false;
         var queuesReady = false;
 
-        const options = {};
-        options.detached = true;
+        // const options = {};
+        // options.detached = true;
 
-        options.listeners = {
-            stdout: (data) => {
-                console.log("STDOUT: " + data);
+        // options.listeners = {
+        //     stdout: (data) => {
+        //         console.log("STDOUT: " + data);
 
-                if (data.contains("Azurite Table service is successfully listening")) {
-                    tablesReady = true;
-                }
+        //         if (data.contains("Azurite Table service is successfully listening")) {
+        //             tablesReady = true;
+        //         }
 
-                if (data.contains("Azurite Blob service is successfully listening")) {
-                    blobsReady = true;
-                }
+        //         if (data.contains("Azurite Blob service is successfully listening")) {
+        //             blobsReady = true;
+        //         }
 
-                if (data.contains("Azurite Queue service is successfully listening")) {
-                    queuesReady = true;
-                }
+        //         if (data.contains("Azurite Queue service is successfully listening")) {
+        //             queuesReady = true;
+        //         }
+        //     }
+        // }
+
+        var azuriteProcess = child.spawn('./node_modules/.bin/azurite', {
+            detached: true,
+            stdio: 'ignore',
+            listeners: {
+
             }
-        }
+        });
 
-        exec.exec('./node_modules/.bin/azurite');
+        azuriteProcess.stdout.on('data', (data) => {
+            console.log("STDOUT: " + data);
+
+            if (data.contains("Azurite Table service is successfully listening")) {
+                tablesReady = true;
+            }
+
+            if (data.contains("Azurite Blob service is successfully listening")) {
+                blobsReady = true;
+            }
+
+            if (data.contains("Azurite Queue service is successfully listening")) {
+                queuesReady = true;
+            }
+        });
 
         await waitUntil(
             () => tablesReady && blobsReady && queuesReady,
             {
                 timeout: startTimeout
             });
+
+        azuriteProcess.unref();
     }
     catch (error) {
         core.setFailed(error.message);
