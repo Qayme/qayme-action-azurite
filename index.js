fd295d37d40016dbca12c8e1dd06f99389da56dc
Lukas Holota <l.holota@outlook.com>
2022-06-14 17:03:47 +0200

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
+const { waitUntil } = require('async-wait-until/dist/commonjs');
 
 async function run() {
     try {
