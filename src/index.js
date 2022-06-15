266e81af55a1441188ae985b44fd3c3002004951
LH <l.holota@outlook.com>
2022-06-15 10:37:58 +0200

    feat: Post action clean up (#5)

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ const exec = require('@actions/exec');
 
 const { waitUntil } = require('async-wait-until/dist/commonjs');
 const { isReady } = require("./readiness");
+const { getContainerName } = require("./container-name");
 
 async function run() {
     try {
         const startTimeout = parseInt(core.getInput('start-timeout')) * 1000;
         const imageTag = core.getInput('image-tag');
 
+        const containerName = getContainerName();
         const fullImageName = `mcr.microsoft.com/azure-storage/azurite:${imageTag}`
 
         // This is to avoid countint the time to pull the image into the start timeout
         await exec.exec(`docker pull ${fullImageName}`);
-        await exec.exec(`docker run -d -p 10000:10000 -p 10001:10001 -p 10002:10002 --name azurite ${fullImageName} azurite --blobHost 0.0.0.0 --tableHost 0.0.0.0 --queueHost 0.0.0.0`);
+        await exec.exec(`docker run -d --name ${containerName} -p 10000:10000 -p 10001:10001 -p 10002:10002 --name azurite ${fullImageName} azurite --blobHost 0.0.0.0 --tableHost 0.0.0.0 --queueHost 0.0.0.0`);
 
         try {
             await waitUntil(
